4f222cd6a030193034741fcf2645302fc1f2e70c
Phuc Hoang <chphuc05@gmail.com>
2021-06-04 13:18:13 -0700

    disable heroku database

--- api1/settings.py
+++ api1/settings.py
@@ -143,6 +143,6 @@ STATICFILES_DIRS = (
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-django_heroku.settings(locals())
+django_heroku.settings(locals(), databases=False)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
