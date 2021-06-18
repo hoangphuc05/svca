f7b5ea14e7b5ad9f686df48f91213a8c5335b79b
Phuc Hoang <chphuc05@gmail.com>
2021-06-18 02:19:50 -0700

    add default secret key

--- api1/settings.py
+++ api1/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY' ,'django-insecure-aa5x2kr6%$*wme-ci3&ol9-%bm2ij_g)pmhilaa-!d7q(ul#(j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
