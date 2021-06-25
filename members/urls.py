426374e433ba939f7853ebdc2fcf5fe6de794b60
Phuc Hoang <chphuc05@gmail.com>
2021-06-25 13:33:18 -0700

    summary

--- members/urls.py
+++ members/urls.py
@@ -7,7 +7,7 @@ from . import views, login, email_handle, need
 router = routers.DefaultRouter()
 router.register(r'members', views.ReactMemberViewSet)
 router.register(r'needs', views.ReactNeedFullViewSet)
-router.register(r'needs-limited', views.ReactNeedSummaryViewSet)
+router.register(r'needs-summary', views.ReactNeedSummaryViewSet)
 
 urlpatterns = [
 
